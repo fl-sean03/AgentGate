4a638c850233ea2bab4c2248a1bb0a00e5d21271
AgentGate E2E Test <agentgate-test@example.com>
2025-12-31 17:09:42 -0500

    [agentgate] WIP before run

--- packages/server/test/integration-service.test.ts
+++ packages/server/test/integration-service.test.ts
@@ -42,7 +42,6 @@ describe('Integration Service', () => {
 
     // Create work order store instance
     workOrderStore = new WorkOrderStore();
-    await workOrderStore.init();
 
     // Create integration service
     integrationService = new IntegrationService(workOrderStore, testDir);
